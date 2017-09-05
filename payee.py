dcabc9fcfc35eadf99aa2e755d27e936259750e1
Georges Labrèche <georges@opendatakosovo.org>
2017-09-06 01:57:52 +0200

    renamed id fields

--- payee.py
+++ payee.py
@@ -1,7 +1,7 @@
 from wtforms import Form, StringField, TextAreaField, IntegerField
 
 class Payee(Form):
-    id = IntegerField('ID or Passport Number:')
+    pid = IntegerField('ID or Passport Number:')
     first_name = StringField('First Name:')
     last_name = StringField('Last Name:')
     father_name = StringField('Father Name:')
--- transaction.py
+++ transaction.py
@@ -1,7 +1,7 @@
 from wtforms import Form, StringField, IntegerField, FloatField, DateTimeField
 
 class Transaction(Form):
-    id = IntegerField('Bank Document ID:')
+    tid = IntegerField('Bank Document ID:')
     receipt_date = DateTimeField('Receipt Date:', format='%d/%m/%y')
     account_type  = StringField('Account Type:')
     amount = FloatField('Amount:')
